d2513cd3f089e00925e70218646c4117dc1126cd
BrianJVarley <brianvarley9@gmail.com>
2015-03-06 00:00:10 +0000

    Mocking device, unit testing 6/3/15

--- Tests/MyoDeviceTests.cs
+++ Tests/MyoDeviceTests.cs
@@ -5,22 +5,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MyoSharp.Poses;
+using MyoSharp.Device;
 
 namespace MyoTestv4.Tests
 {
     [TestFixture]
-    class MyoDeviceTests
+    class MyoDeviceTests 
     {
 
-        MyoTestv4.AdductionAbductionFlexion.MyoDeviceModel myo = new AdductionAbductionFlexion.MyoDeviceModel();
 
         [Test]
-        public void TestMyoDevicePoseChanged()
+        public void MockTest()
         {
             
             //unit test the Myo device pose changed code
-            //myo.PoseChanged += Myo_PoseChanged;
             //need to mock device to test this.
+            var mockMyo = new MyoMock();
+            //var device = new myode(fakeMyo);
+            mockMyo.OnMockEvent();
+            //Assert.AreEqual(1, device.Count);
 
 
         }
---
+++ Tests/MyoMock.cs
@@ -0,0 +1,25 @@
+﻿using MyoSharp.Device;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyoTestv4.Tests
+{
+    class MyoMock : IMyo
+    {
+
+        public event EventHandler MockEvent;
+
+
+        public void OnMockEvent()
+        {
+            var handler = MockEvent;
+            if (handler != null) handler(this, EventArgs.Empty);
+        }
+
+
+
+    }
+}
